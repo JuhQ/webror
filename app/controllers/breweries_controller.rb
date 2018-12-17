4d8dd92a1e057dcecdfac125ccc2900993c1dd3f
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-17 17:49:18 +0200

    Render panimot

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -5,7 +5,7 @@ class BreweriesController < ApplicationController
   # GET /breweries.json
   def index
     @breweries = Brewery.all
-    render :index   # renderöi hakemistossa view/breweries olevan näkymätemplaten index.html.erb
+    render :panimot
   end
 
   # GET /breweries/1
