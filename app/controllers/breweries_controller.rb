512d7f87781135c69a0fdeccc0b12b9343c88292
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-17 17:32:31 +0200

    Edit view template

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -5,6 +5,7 @@ class BreweriesController < ApplicationController
   # GET /breweries.json
   def index
     @breweries = Brewery.all
+    render :index   # renderöi hakemistossa view/breweries olevan näkymätemplaten index.html.erb
   end
 
   # GET /breweries/1
