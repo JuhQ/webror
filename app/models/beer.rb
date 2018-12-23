e93284b634fd40ba6a1ba79a4a594e748ed64298
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-24 01:39:15 +0200

    Count beer rating average

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    avg = 0
+    for rating in ratings do
+      avg += rating.score
+    end
+    avg / ratings.count
+  end
+
 end
