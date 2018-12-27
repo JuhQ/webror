94e764b3997c080bbea73a13de17de1888368c6c
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-27 23:18:57 +0200

    Add ratings to brewery page

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
@@ -10,6 +11,10 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2018
     puts "changed year to #{year}"
+  end
+
+  def average_rating
+    ratings.average(:score)
   end
 
   def to_s
