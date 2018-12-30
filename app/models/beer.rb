ca2dda87a8079a8eaa05b71c051a5a47fd3629e7
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2018-12-30 20:30:11 +0200

    Add more validation

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,7 @@ class Beer < ApplicationRecord
 
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  validates :name, presence: true
 
   def average
     return 0 if ratings.empty?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   include RatingAverage
   validates :username, uniqueness: true, length: { minimum: 3 }
+
+  validates :username, length: { minimum: 3,
+                                 maximum: 30 }
+
   has_many :ratings # käyttäjällä on monta ratingia
 end
