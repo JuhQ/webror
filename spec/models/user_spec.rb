b9d3b7caac4514641af6c4d2306f45c302108caa
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2019-01-05 18:08:28 +0200

    Improve testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,14 +54,11 @@ RSpec.describe User, type: :model do
     end
 
     it "is the one with highest rating if several rated" do
-      beer1 = FactoryBot.create(:beer)
-      beer2 = FactoryBot.create(:beer)
-      beer3 = FactoryBot.create(:beer)
-      rating1 = FactoryBot.create(:rating, score: 20, beer: beer1, user: user)
-      rating2 = FactoryBot.create(:rating, score: 25, beer: beer2, user: user)
-      rating3 = FactoryBot.create(:rating, score: 9, beer: beer3, user: user)
+      create_beer_with_rating({ user: user }, 10 )
+      create_beer_with_rating({ user: user}, 7 )
+      best = create_beer_with_rating({ user: user }, 25 )
 
-      expect(user.favorite_beer).to eq(beer2)
+      expect(user.favorite_beer).to eq(best)
     end
   end
 
@@ -80,5 +77,43 @@ RSpec.describe User, type: :model do
       expect(user.ratings.count).to eq(2)
       expect(user.average_rating).to eq(15.0)
     end
+  end
+
+  describe "favorite beer" do
+    let(:user){ FactoryBot.create(:user) }
+
+    it "has method for determining one" do
+      expect(user).to respond_to(:favorite_beer)
+    end
+
+    it "without ratings does not have one" do
+      expect(user.favorite_beer).to eq(nil)
+    end
+
+    it "is the only rated if only one rating" do
+      beer = FactoryBot.create(:beer)
+      rating = FactoryBot.create(:rating, score: 20, beer: beer, user: user)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it "is the one with highest rating if several rated" do
+      create_beers_with_many_ratings({user: user}, 10, 20, 15, 7, 9)
+      best = create_beer_with_rating({ user: user }, 25 )
+
+      expect(user.favorite_beer).to eq(best)
+    end
+  end
+end
+
+def create_beer_with_rating(object, score)
+  beer = FactoryBot.create(:beer)
+  FactoryBot.create(:rating, beer: beer, score: score, user: object[:user] )
+  beer
+end
+
+def create_beers_with_many_ratings(object, *scores)
+  scores.each do |score|
+    create_beer_with_rating(object, score)
   end
 end
