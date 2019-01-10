0d3caa24b41c77b19ba903ee216293435a6fe795
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2019-01-10 16:14:26 +0200

    Add view for place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,19 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    @place = session["places-#{params[:id]}"]
+  end
+
   def search
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
+      @places.each do |place|
+        session["places-#{place.id}"] = place
+      end
+
       render :index
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
+  resources :places, only: [:index, :show]
 
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
