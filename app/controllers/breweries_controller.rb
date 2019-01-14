89feaa464edeccabcd1751d4b787240d0ed40868
Juha Tauriainen <juha.tauriainen@houston-inc.com>
2019-01-15 00:25:15 +0200

    Make sure only admins can remove stuff

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,6 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_admin, only: [:destroy]
   before_action :ensure_that_signed_in, except: [:index, :show]
 
   # GET /breweries
